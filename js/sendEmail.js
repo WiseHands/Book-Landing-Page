6d8f5df829111f5b61f902ff4f7e93fec1716098
Taras H <research.010@gmail.com>
2019-12-05 21:23:19 +0200

    email validation

--- js/sendEmail.js
+++ js/sendEmail.js
@@ -1,3 +1,17 @@
+let mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+function ValidateEmail (inputText) {
+
+    if(inputText.value.match(mailformat)) {
+        document.getElementById('email').focus();
+        return true;
+    } else {
+        alert("Ви ввели неправильну e-mail адресу!");
+        document.getElementById('email').focus();
+        return false;
+    }
+}
+
 function postNewEmail(data) {
 
     let email = document.getElementById('email').value;
@@ -5,25 +19,29 @@ function postNewEmail(data) {
     let params = '?email=' + email;
 
     let apiUrl = '/api' + params;
-    fetch(apiUrl, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            body: data
-        }
-    }).then(function(response){
-        if(response.ok) {
-            document.querySelector('.send-email').style.display = 'none';
-            document.querySelector('.mail-button').style.display = 'none';
-            document.querySelector('.send-email-success').style.display = 'block';
-        } else {
-            document.querySelector('.send-email').style.display = 'none';
-            document.querySelector('.send-email-error').style.display = 'block';
-        }
-        console.log(response)
-        return  response.json();
-    }).then(function(data) {
-        console.log(data);
-    })
+    if (email.match(mailformat)) {
+        fetch(apiUrl, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                body: data
+            }
+        }).then(function (response) {
+            if (response.ok) {
+                document.querySelector('.send-email').style.display = 'none';
+                document.querySelector('.mail-button').style.display = 'none';
+                document.querySelector('.send-email-success').style.display = 'block';
+            } else {
+                document.querySelector('.send-email').style.display = 'none';
+                document.querySelector('.send-email-error').style.display = 'block';
+            }
+            console.log(response)
+            return response.json();
+        }).then(function (data) {
+            console.log(data);
+        })
+    } else {
+        console.log("email is not match the pattern");
+    }
 
 }
